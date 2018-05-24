19abb881e25d490ad32a7b0f22c2e12fe5732278
tatsunode <tatsunode@yahoo.co.jp>
2018-05-24 19:05:24 +0900

    ITFFFからのRealtime Databaseの更新

--- IFTTT-Firebase-Function/functions/index.js
+++ IFTTT-Firebase-Function/functions/index.js
@@ -1,8 +1,37 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+var config = {}
+config.apiKey = functions.config().db.apikey;
+config.authDomain = functions.config().db.authdomain;
+config.databaseURL = functions.config().db.databaseurl;
+config.projectId = functions.config().db.projectid;
+config.storageBucket = functions.config().db.storagebucket;
+
+firebase.initializeApp(config);
+
+exports.iftttToRealtimeDatabase = functions.https.onRequest((request, response) => {
+
+    const email = request.body.email;
+    const userid = request.body.userid;
+    const topic = request.body.topic;
+    const value = request.body.value;
+    const password = functions.config().account.password;
+    const secret =   functions.config().account[userid].secret;
+
+    if (request.body.secret != secret) {
+        response.send("error");
+        return;
+    }
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(user => {
+            firebase.database().ref("/mqtt/" + topic).set("").then(function() {
+                firebase.database().ref("/mqtt/" + topic).set(value);
+                response.send("topic: " + value);
+            })
+        })
+        .catch(error => {
+            response.send("error");
+        });
+});
